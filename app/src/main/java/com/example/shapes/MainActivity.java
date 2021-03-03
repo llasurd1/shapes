04efcb466b8b3fd3c4b28e9465d53d2f2597374f
llasurd1 <llasurdo@gmail.com>
2021-03-03 13:46:01 -0500

    figured out the shapes

--- app/src/main/java/com/example/shapes/MainActivity.java
+++ app/src/main/java/com/example/shapes/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.shapes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new MyView(this));
     }
+    public class MyView extends View {
+        Paint paint  = null;
+        public MyView(Context context) {
+            super(context);
+            paint = new Paint();
+        }
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            int x = getWidth();
+            int y = getHeight();
+            int radius;
+            radius = 100;
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.WHITE);
+            canvas.drawPaint(paint);
+            // Use Color.parseColor to define HTML colors
+            paint.setColor(Color.parseColor("#FF0000"));
+            canvas.drawCircle(300, 300, 100, paint);
+            canvas.drawLine(20, 0, 500, 500 , paint);
+        }
+
+       }
+
 }
